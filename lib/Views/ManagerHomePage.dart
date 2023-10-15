53da172c3efc571d1948bd6d25ed72be2ec1e217
Jhen Mu <terminal201467@gmail.com>
2023-10-16 07:01:35 +0800

    新增管理者、志工HomePage

---
+++ lib/Views/ManagerHomePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//如果是管理者的話
+  ///1. 登入看志工的打卡狀態
+  ///2. 變成自己是一個據點，讓志工可以自己掃描QRCode
+  ///3. 管理者自己的狀態頁面
+  ///4. 輸出文件檔Button
+
+class ManagerHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/Views/VolunteerHomePage.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+///如果是志工的話
+  ///1. 可以點選要打開相機的Button，打開QRCode掃描頁面
+  ///2. 志工使用的狀態頁面
+  ///3. 
+
+class VolunteerHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
