10fe2d72b56eecd2378e38eae086e65e0c6625bf
Jhen Mu <terminal201467@gmail.com>
2023-10-19 08:23:15 +0800

    新增Provider套件、開始設計志工首頁ViewModel

---
+++ lib/ViewModel/VolunteerHomePageViewModel.dart
@@ -0,0 +1,26 @@
+import 'package:provider/provider.dart';
+
+class VolunteerHomePageViewModel {
+
+  //提供志工性名
+  String getVolunteerName() {
+    return '';
+  }
+
+  //今日簽到
+  void signIn() {
+
+  }
+
+  //登出
+  void signOut() {
+
+  }
+
+  //目前已儲存服務天
+
+  //本月服務天數
+
+  //協會通知
+
+}
