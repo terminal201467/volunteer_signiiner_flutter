1b5a407a3032be43e7eb419475741c71b1fd1f70
Jhen Mu <terminal201467@gmail.com>
2023-11-09 08:25:46 +0800

    更新UserManager的CRUD功能

--- lib/ViewModel/UserManager.dart
+++ lib/ViewModel/UserManager.dart
@@ -1,12 +1,14 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:volunteer_signiiner_flutter/Model/User.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 enum LoginState {
   loggedIn, 
   notLoggedIn, 
 }
+
+typedef ErrorCallback = void Function(String errorMessage);
 
 class UserManager {
 
@@ -21,8 +23,6 @@ List<int> serveDates = [];
 List<int> serveMonthDates = [];
 
 LoginState currentLoginState = LoginState.notLoggedIn;
-
-// typedef ErrorCallback = void Function(String errorMessage);
 
 void initState() {
   getUserData();
@@ -78,25 +78,53 @@ void createUserData(
     errorCallback('UID 是必填項目，請填入 UID！');
     return;
   }
-
-  
-  
 }
 
 
 // 讀取使用者資料
-void getUserData() {
+void getUserData(String id) {
 
-  }
+}
 
 // 更改使用者資料
-void changeUserDate() {
-
+void updateUserData(
+  String? gender,
+  String? identity,
+  String? name,
+  String? phoneNumber,
+  String? signUpTimeStamp,
+  String? uid,
+  ErrorCallback errorCallback,
+  ) {
+  // 依次检查每个属性，如果传入了新的值，则执行相应的更改操作
+  if (gender != null) {
+    // 执行更改性別的操作
   }
+
+  if (identity != null) {
+    // 执行更改身份證字號的操作
+  }
+
+  if (name != null) {
+    // 执行更改名字的操作
+  }
+
+  if (phoneNumber != null) {
+    // 执行更改電話號碼的操作
+  }
+
+  if (signUpTimeStamp != null) {
+    // 执行更改註冊時間戳記的操作
+  }
+
+  if (uid != null) {
+    // 执行更改 UID 的操作
+  }
+}
 
 // 刪除使用者資料
-void destroyUserData() {
+void destroyUserData(String id) {
 
-  }
+}
 
 }
